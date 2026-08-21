482df18c718b065f8b587f5736efd2af76625b37
Renato176 <renato.coelhoneto2007@gmail.com>
2026-08-20 22:22:59 -0300

    SomaDosNumero.py

---
+++ SomaDosNumero.py
@@ -0,0 +1,27 @@
+'''
+# A ideia e fazer a soma de dois números de valores diferente e soma 
+'''
+
+primero = int(input("Primeiro valor:"))
+segundo = int(input("Segundo valor:"))
+
+soma= primero + segundo
+
+print(f"Soma: {soma}")
+
+'''
+# teste 1
+# Primeiro valor: 8
+# Segundo valor: -3
+# Soma: 5
+
+# teste 2
+# Primeiro valor: 4
+# Segundo valor: 9
+# Soma: 13
+
+# teste 3
+# Primeiro valor: 0
+# Segundo valor: 12
+# Soma: 12
+'''
