0300e3fb62b1f94cd013d37d7d1bec85c4a8a787
Renato176 <renato.coelhoneto2007@gmail.com>
2026-09-16 19:09:00 -0300

    Delete CustoFinaldaCompra.py

--- CustoFinaldaCompra.py
+++
@@ -1,6 +0,0 @@
-Preco = float(input("Preço unitário:"))
-Quant = int(input("Quantidade:"))
-Frete = float(input("Frete:"))
-
-print(f"Subtotal: {Preco * Quant}")
-print(f"Total: {(Preco * Quant) + Frete}")
